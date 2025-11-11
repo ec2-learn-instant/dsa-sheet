93c81efe45e9e8793b5d96b787e44883dfff447b
Sakthivel Govinthan <sakthiveltechit@gmail.com>
2025-11-11 21:42:40 +0530

    Remove unwanted page in code

--- dsa-sheet-frontend/src/App.js
+++ dsa-sheet-frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
 import Topics from "./pages/Topics";
 import Progress from "./pages/Progress";
 import Profile from "./pages/Profile";
@@ -28,12 +27,11 @@ const App = () => {
             <PrivateRoute>
               <Layout>
                 <Routes>
-                  <Route path="/dashboard" element={<Dashboard />} />
                   <Route path="/topics" element={<Topics />} />
                   <Route path="/progress" element={<Progress />} />
                   <Route path="/profile" element={<Profile />} />
                   {/* Default redirect to dashboard */}
-                  <Route path="*" element={<Navigate to="/dashboard" />} />
+                  <Route path="*" element={<Navigate to="/topics" />} />
                 </Routes>
               </Layout>
             </PrivateRoute>
